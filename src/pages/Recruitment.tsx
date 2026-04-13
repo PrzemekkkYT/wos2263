25ec9f144d956e2a4b3d46f32804b8d1853d291a
Przemek <przemekytmail@gmail.com>
2026-04-13 22:14:14 +0200

    yhm

--- src/pages/Recruitment.tsx
+++ src/pages/Recruitment.tsx
@@ -4,15 +4,12 @@ import { alliances } from "@/data/alliances";
 export function RecruitmentPage() {
   return (
     <main>
-      <section>
-        <h1 class="relative z-10 max-w-7xl mx-auto px-6 w-full text-center text-5xl xl:text-[7rem] font-bold animate-fade-down">
-          Recruitment board
+      <section class="mt-24">
+        <h1 class="relative z-10 max-w-7xl mx-auto px-6 w-full text-center text-8xl xl:text-[10rem] font-bold animate-fade-down">
+          Alliances
         </h1>
-        <span class="text-xl xl:text-2xl z-10 mt-8 mb-2">
-          Explore our alliances, contact our recruiters
-        </span>
       </section>
-      <section class="mt-24 px-0!">
+      <section class="px-0!">
         <div class="flex flex-col gap-8 w-11/12">
           {alliances.map(
             (al) => al.recruitment && <AllianceView alliance={al} />,
